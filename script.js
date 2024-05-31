b5f39d0ff2bc750ec6cf097a9a8a237b7e2e2c43
S4lvat0r3 <jacyanh@gmail.com>
2024-05-31 15:27:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const starsBackground = document.getElementById('stars-background');
+    starsBackground.classList.remove('hidden');
+});
